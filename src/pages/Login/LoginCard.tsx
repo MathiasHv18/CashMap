1f49f1e37eed394aed5529aeda12a5f860accf2a
Mathias <mathiashv2014@gmail.com>
2024-12-20 01:24:36 -0500

    Center Div login Card

--- src/pages/Login/LoginCard.tsx
+++ src/pages/Login/LoginCard.tsx
@@ -1,6 +1,6 @@
 import "./LoginCard.css";
 import frasesLogin from "../../assets/frasesLogin.json";
-import { LineChart, Line, ResponsiveContainer } from 'recharts'
+import { LineChart, Line, ResponsiveContainer } from "recharts";
 
 const data = [
   { expense: 1000 },
@@ -10,29 +10,37 @@ const data = [
   { expense: 800 },
   { expense: 1100 },
   { expense: 1300 },
-]
+];
 
 function LoginCard() {
   const randomIndex = Math.floor(Math.random() * frasesLogin.messages.length);
   return (
-    <div className="loginCard">
-      <h2>{frasesLogin.messages[randomIndex]}</h2>
-      <div className="chartContainer">
-        <ResponsiveContainer width="100%" height={100}>
-          <LineChart data={data}>
-            <Line type="monotone" dataKey="expense" stroke="#3b82f6" strokeWidth={2} dot={false} />
-          </LineChart>
-        </ResponsiveContainer>
+    <div className="outerContainer">
+      <div className="loginCard">
+        <h2 className="tittle">{frasesLogin.messages[randomIndex]}</h2>
+        <div className="chartContainer">
+          <ResponsiveContainer width="100%" height={100}>
+            <LineChart data={data}>
+              <Line
+                type="monotone"
+                dataKey="expense"
+                stroke="#3b82f6"
+                strokeWidth={2}
+                dot={false}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+        <input type="text" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button className="button">Login</button>
+        <span>
+          <a href="">I forgot my password</a>
+        </span>
+        <span>
+          Don't have an account? <a href="">Register</a>
+        </span>
       </div>
-      <input type="text" placeholder="Email" />
-      <input type="password" placeholder="Password" />
-      <button className="button">Login</button>
-      <span>
-        <a href="">I forgot my password</a>
-      </span>
-      <span>
-        Don't have an account? <a href="">Register</a>
-      </span>
     </div>
   );
 }
