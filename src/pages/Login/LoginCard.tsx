6de9cab4a228a5162d5e5510d730d46ba86a5ca1
Mathias <mathiashv2014@gmail.com>
2024-12-20 00:45:58 -0500

    curva

--- src/pages/Login/LoginCard.tsx
+++ src/pages/Login/LoginCard.tsx
@@ -6,6 +6,18 @@ function LoginCard() {
   return (
     <div className="loginCard">
       <h2>{frasesLogin.messages[randomIndex]}</h2>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 200 50"
+        className="loginWave"
+      >
+        <path
+          d="M0 40 Q50 10 90 30 T200 10"
+          fill="none"
+          stroke="#3b82f6"
+          strokeWidth="2"
+        />
+      </svg>
       <input type="text" placeholder="Email" />
       <input type="password" placeholder="Password" />
       <button className="button">Login</button>
