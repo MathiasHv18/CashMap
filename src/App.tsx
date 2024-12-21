008aef4f9d7dadfd058879e87345fd9ff3fa804c
Mathias <mathiashv2014@gmail.com>
2024-12-21 00:41:09 -0500

    Quick fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginCard from "./pages/Login/LoginCard";
 import RegisterPage from "./pages/Register/RegisterPage";
+import AppMain from "./pages/appMain/AppMain";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/" element={<LoginCard />} />
+        <Route path="/mainPage" element={<AppMain />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/appMain/AppMain.tsx
@@ -0,0 +1,14 @@
+function AppMain() {
+  return (
+    <div className="outerBox">
+      <h1>Análisis de Gastos</h1>
+      <div className="mainContainer">
+        <div className="input">Lasfpioasgfjio</div>
+        <div className="resumen">sdfsdggsd</div>
+        <div className="chart">sdgsdg</div>
+      </div>
+    </div>
+  );
+}
+
+export default AppMain;
