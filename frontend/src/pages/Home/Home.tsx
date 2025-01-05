e9fe70d863aaf4e8abccf0f316846c08186921f2
Mathias <mathiashv2014@gmail.com>
2025-01-05 18:56:21 -0500

    Avance inputBox

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -7,21 +7,21 @@ function Home() {
       <h1 className="Home_tittle">Expense analysis</h1>
       <div className="Home_inputBox">
         <h2 className="Home_subtittle">Insert your income </h2>
-        <h4 className="Home_h4">Add your expenses for analysis</h4>
+        <h5 className="Home_h5">Add your expenses for analysis</h5>
         <h3 className="Home_h3">Concept</h3>
-        <input className="Home_input" type="text" />
+        <input className="Home_input" type="text" required />
         <h3 className="Home_h3">Amount</h3>
         <input className="Home_input" type="number" />
         <h3 className="Home_h3">Date</h3>
         <input className="Home_input" type="date" />
         <h3 className="Home_h3">Category</h3>
         <input className="Home_input" type="text" /> <br />
-        <button>Add</button>
+        <button className="Home_button">Add</button>
       </div>
-      <div className="Home_history">
+      <div className="Home_historyBox">
         <h2 className="Home_subtittle">History</h2>
       </div>
-      <div className="Home_stats">
+      <div className="Home_statsBox">
         <h2 className="Home_subtittle">Stats</h2>
       </div>
     </div>
