07bce4017987cf3c1da541990f849c75530ed2b6
Mathias <mathiashv2014@gmail.com>
2025-01-17 23:56:07 -0500

    Fix Cors problem

---
+++ backend/src/main/java/com/cashmap/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.cashmap.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Permitir todas las rutas
+                .allowedOrigins("*") // Permitir todos los orígenes
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("*") // Permitir todos los encabezados
+                .allowCredentials(false); // Las credenciales no se permiten con "*"
+    }
+}
