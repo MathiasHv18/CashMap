3f56e78c1b5c4fa337d55b7af82f0637016777dc
Mathias <mathiashv2014@gmail.com>
2025-03-23 17:58:27 -0500

    fixed nav bar

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -14,17 +14,19 @@ function Home() {
 
   return (
     <div className="w-full">
-      <nav>
-        <h1 className="text-3xl font-bold tracking-wide">
-          {user?.name}'s Expense Analysis
+      <div className="w-full top-10 fixed mb-30">
+        <h1 className="text-3xl font-bold tracking-wide mb-2">
+          {user?.name}'s Economy
         </h1>
-      </nav>
-      <div className="w-full">
-        <Navbar onSelect={setSelectedTab} />
-        <div className="p-6">
-          {selectedTab === "Summary" && <Summary />}
-          {selectedTab === "Transaction" && <Transaction />}
+        <div className="mb-30 ">
+          <div className="bg-white ">
+            <Navbar onSelect={setSelectedTab} />
+          </div>
         </div>
+      </div>
+      <div className="p-6">
+        {selectedTab === "Summary" && <Summary />}
+        {selectedTab === "Transaction" && <Transaction />}
       </div>
     </div>
   );
